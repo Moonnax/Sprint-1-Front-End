dba9afc7f0a0d53f92e72f962cb159f3deec8440
Isabely Marques <isabelymarques844@gmail.com>
2025-11-09 22:03:58 -0300

    validação de nome, email e telefone

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 const form = document.querySelector(".form");
+
+const apenasTexto = /^[A-Za-zÀ-ÿ\s]+$/;
+const validarEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const telMinimo = (tel) => {
+  let numeros = 0;
+  for (let i = 0; i < tel.length; i++) {
+    if (tel[i] >= '0' && tel[i] <= '9') numeros++;
+  }
+  return numeros >= 11;
+};
